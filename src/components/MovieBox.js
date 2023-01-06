e8fe0e07e46caf5ac4dcd49f527e3e7a5c64129a
Sathis <rsathisk15@gmail.com>
2023-01-06 01:08:57 +0100

    axios installed and mapped to Movie box

--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import StarRateIcon from '@mui/icons-material/StarRate';
-const MovieBox = () => {
+const MovieBox = ({ data }) => {
+  console.log(data.vote_average);
   return (
     <div className="movie-box">
       <img
-        src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQnEi3TKfHxsAvT04Paw-a3rYUjRs19be4xI-Y2uI5DbLz-rft4"
+        src={`https://image.tmdb.org/t/p/w1280${data.poster_path}`}
         alt="Spyderman"
       />
       <div className="movie-details">
-        <div className="name">Spyder-Man</div>
+        <div className="name">{data.original_title || data.name}</div>
         <div className="details">
-          <div className="type">Movie</div>
-          <div className="date">12/10/2023</div>
+          <div className="type" style={{ textTransform: 'uppercase' }}>
+            {data.media_type == 'tv'
+              ? data.media_type + ' Series'
+              : data.media_type}
+          </div>
+          <div className="date">{data.release_date || data.first_air_date}</div>
         </div>
         <div className="rating">
           <StarRateIcon />
--- src/components/MovieContent.js
+++ src/components/MovieContent.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import MovieBox from './MovieBox';
 
 const MovieContent = () => {
+  const [movieData, setMovieData] = useState([]);
+  useEffect(() => {
+    async function x() {
+      let data = await axios.get(
+        'https://api.themoviedb.org/3/trending/all/day?api_key=20c575bf9f44538421f08161da824a42'
+      );
+      setMovieData(() => data.data.results);
+    }
+    x();
+  }, []);
   return (
     <div className="movie-content">
-      <MovieBox />
-      <MovieBox />
-      <MovieBox />
-      <MovieBox />
-      <MovieBox />
+      {movieData.map((data) => (
+        <MovieBox key={data.id} data={data} />
+      ))}
     </div>
   );
 };
